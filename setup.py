fcd7c675558c96501cbad517125e579921364c13
Junwoo Hwang <junwoo091400@gmail.com>
2022-12-06 14:44:59 +0100

    setup.py: Only include 'windywings' as package to build
    - Without this, pip is confused when installing the package, thinking
      the 'simulations' is also another package to install, which isn't the
      case.
    - Relevant documentation:
      https://setuptools.pypa.io/en/latest/userguide/package_discovery.html#automatic-discovery

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 setup(    
     name="windywings-gym",
     version='0.0.1',
+    packages=["windywings"],
     install_requires=['gym[classic_control]',
                       'numpy',
                       'matplotlib',
