a452b4eba829d1f7adc730a921f93374c1462e8a
Junwoo Hwang <junwoo091400@gmail.com>
2023-02-16 23:08:15 +0100

    VelRef: Add NPFG Squashed model (v1)
    - Currently not sure how to handle V_nom situation, so some arbitray
      constants to set minimum value of V_nom, based on V_max is added

--- theories/velocity_reference_algorithms.py
+++ theories/velocity_reference_algorithms.py
@@ -336,36 +336,66 @@ class TjNpfgBearingFeasibilityStripped(TjNpfg):
         # doesn't get set. So we need to cache the bound independently!!
         return self.track_error_bound
 
-class RuckigTimeOptimalTrajectory(VelocityReferenceCurves):
+class TjNpfgBearingFeasibilityStrippedVpathSquashed(TjNpfg):
     '''
-    Utilizes the `ruckig` package from the paper: "Jerk-limited Real-time Trajectory Generation with Arbitrary Target States" (http://arxiv.org/abs/2105.04830)
+    This is a simplified implementation of TJ's NPFG, with the bearing feasibility functionality stripped & taking V_path into account
+    by squashing the V_nom (needs to be set to *nominal speed, can't just be 0!!!)
 
-    This library creates a bang-bang controller type (on Jerk term) trajectory that guarantees a time-optimal
-    trajectory that reaches the target condition.
-
-    Goal of the trajectory:
-    - As the 'parallel' position (distance) on the path as a target is undefined (we can freely choose), we simply
-    choose to reach the 'target speed on path' in optimal time, along with coming to a stop in orthogonal direction.
-    - Therefore, target pos = {track error, UNDEFINED} and target vel = {0, path speed target}, in orthogonal/parallel terms each.
+    Because the feasibility function interferes with setting the desired minimum ground speed (when using track-keeping),
+    so it has a dependency to vehicle's real-time ground speed (as that affects feasibility output).
     
-    Limitations:
-    - Jerk limit is applied to each axis, hence the max jerk is actually sqrt(2) * Jerk, in most cases actually (But could be improved on time synchronization step by modifying the library)
+    Since V_nom must be set high enough, the track-keeping itself wouldn't affect velocity curve, unless there is a strong
+    wind.
 
-    Findings:
-    - The 'initial' condition is actually not well defined, depending on the trajectory we can always have different speed/acceleration as an initial condition
-    - One method to bypass this would be to consider that at the 'track error boundary', the vehicle is at it's max approach speed & 0 acceleration
-    - This will then formulate the whole vector field, and trajectory won't be generated for each different track error conditions, but would simply provide output
-    of the look-up table formulated initially from the track error boundary condition assumption.
+    NOTE: The squashing mechanism must adapt if there's a high wind, but for now we assume negligible wind
+    and thus squash it via the ratio of: V_path/V_nom.
     '''
-    def __init__(self, vel_range, max_acc, max_jerk):
-        super().__init__(vel_range, max_acc, max_jerk)
+    def __init__(self, vel_range, ground_speed, track_keeping_speed):
+        # Enforced arbitrary ratio to keep to satisfy the assumption (and squashing) to work
+        V_NOM_TO_V_MAX_MIN_RATIO = 0.5
+        # Hard lower limit the nominal airspeed
+        if vel_range[1] < vel_range[2] * V_NOM_TO_V_MAX_MIN_RATIO:
+            print('[TJ NPFG Squashed] V_nom too low! Vel Range encountered:', vel_range)
+            vel_range = np.copy(vel_range) # Copy new data, so we don't modify original vel range
+            vel_range[1] = vel_range[2] * V_NOM_TO_V_MAX_MIN_RATIO
 
-        # Calculate the track error boundary directly
-        # We assume vehicle heading orthogonal to the path at MAXIMUM velocity, then slowing down
+        assert vel_range[1] >= vel_range[2] * V_NOM_TO_V_MAX_MIN_RATIO, "V_nom must be bigger than sane value for squashing assumption to work!!"
+        
+        super().__init__(vel_range, ground_speed, track_keeping_speed)
 
     def calculate_velRef(self, track_error, v_path):
-        # Orthogonal velocity follows a simple 
-        return None
+        '''
+        Returns velocity reference, bypassing bearing feasibility calculation
+
+        Basically replaces the `guideToPath_nowind` function.
+        '''
+        self.assert_input_variables(track_error, v_path)
+        v_p_squash_ratio = v_path / self.vel_range[1] # V_path/V_nom
+
+        # Constants
+        NEGATIVE_TRACK_ERROR_AUGMENT = -1.0 # Augmented signed track error to have an effect of vehicle being at 'right' side of the path. Used in `bearingVec` func of NPFG
+        ZERO_FEASIBILITY_COMBINED_AUGMENT = 0.0 # Augmented combined feasibility, to disable effect of feasibility in scaling in `minGroundSpeed` of NPFG
+
+        # skip `adaptPeriod`, `pGain` and `timeConst` function calls
+
+        # LOGIC
+        # TEB calculation with time-constant is left as legacy
+        self.track_error_bound = self.npfg.trackErrorBound(self.ground_speed, self.npfg.time_const)
+        normalized_track_error = np.clip(track_error/self.track_error_bound, 0.0, 1.0)
+        look_ahead_ang = self.npfg.lookAheadAngle(normalized_track_error) # LAA solely based on track proximity (normalized)
+        bearing_vector = self.npfg.bearingVec(PATH_UNIT_TANGENT_VEC, look_ahead_ang, NEGATIVE_TRACK_ERROR_AUGMENT)
+        minimum_groundspeed_reference = self.npfg.minGroundSpeed(normalized_track_error, ZERO_FEASIBILITY_COMBINED_AUGMENT)
+
+        # X, Y component. Should represent Parallel and Orthogonal components of reference velocity curve
+        raw_vel_ref = self.npfg.refAirVelocity(bearing_vector, minimum_groundspeed_reference)
+
+        # Return the 'squashed' velocity curve
+        return np.array([raw_vel_ref[0] * v_p_squash_ratio, raw_vel_ref[1]])
+    
+    def get_track_error_boundary(self):
+        # NOTE: Since BF Stripped doesn't call all necessary functions, the debug value for track error bound actually
+        # doesn't get set. So we need to cache the bound independently!!
+        return self.track_error_bound
 
 class TjNpfgCartesianlVapproachMin(TjNpfg):
     '''
